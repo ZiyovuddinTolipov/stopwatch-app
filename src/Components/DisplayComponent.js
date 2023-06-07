ba851d323d6629f54d9362f9d8519478b1944a79
Tolipov <ziyovuddin.developer@gmail.com>
2023-06-08 03:37:03 +0500

    full App

--- src/Components/DisplayComponent.js
+++ src/Components/DisplayComponent.js
@@ -5,7 +5,9 @@ const DisplayComponent = (props) => {
         if (props.time.h === 0) {
             return '';
         } else {
-            return <span>{(props.time.h >= 10) ? props.time.h : "0" + props.time.h}</span>
+            return <>
+                <span>{(props.time.h >= 10) ? props.time.h : "0" + props.time.h} </span>&nbsp; : &nbsp;
+            </>
         }
 
     }
@@ -13,7 +15,7 @@ const DisplayComponent = (props) => {
         <div>
             <div className='clock-holder'>
                 <div className='stopwatch'>
-                    {h()}&nbsp; : &nbsp;
+                    {h()}
                     <span>{(props.time.m >= 10) ? props.time.m : "0" + props.time.m}</span>&nbsp; : &nbsp;
                     <span>{(props.time.s >= 10) ? props.time.s : "0" + props.time.s}</span>&nbsp; : &nbsp;
                     <span>{(props.time.ms >= 10) ? props.time.ms : "0" + props.time.ms}</span>&nbsp;
